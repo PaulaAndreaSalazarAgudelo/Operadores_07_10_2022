e1198e912e6a2915957c6b17e4d72e8a6691733e
PaulaAndreaSalazarAgudelo <andreasalazaragudelo@gmail.com>
2022-10-19 20:19:24 -0500

    Operadores aritmeticos parte 1

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 addEventListener("DOMContentLoaded", async(e) => {
-    let peticion = await fetch("api.php");
+    let peticion = await fetch("https://paulaandreasalazaragudelo.000webhostapp.com/Operadores_07_10_2022/api.php");
     let datos = await peticion.text();
     document.querySelector("pre").innerHTML = datos;
 })
